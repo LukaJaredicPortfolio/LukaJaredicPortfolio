66898cb33986ef8148a6785f1ee187110301c37b
LukaJaredicPortfolio <155008848+LukaJaredicPortfolio@users.noreply.github.com>
2024-01-13 18:18:17 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 const scrollUpArrowWrapper = document.querySelector('.scrollUpArrowWrapper');
-
-
+const navMediaIcons = document.querySelector('.navMediaIcons');
+document.body.classList.add('loading');
 window.addEventListener('scroll', function() {
     if (window.innerWidth >= 1440) {
         if (window.scrollY > 100) {
-            scrollUpArrowWrapper.style.display = 'block';
-            scrollUpArrowWrapper.style.marginTop = '0px'
+            scrollUpArrowWrapper.classList.remove('scrollUpArrowWrapperHidden')
         } else {
-            scrollUpArrowWrapper.style.display = 'none';
-            scrollUpArrowWrapper.style.marginTop = '200px'
+            scrollUpArrowWrapper.classList.add('scrollUpArrowWrapperHidden')
         }
     }
     else[
-        scrollUpArrowWrapper.style.display = 'none'
+        scrollUpArrowWrapper.classList.add('scrollUpArrowWrapperHidden')
     ]
-    
+
+    const bottomOfViewport = window.scrollY + window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+    if (window.innerWidth >= 980){
+        if (bottomOfViewport > documentHeight - window.innerHeight / 2) {
+            navMediaIcons.classList.add('navMediaHidden');
+          } else {
+            navMediaIcons.classList.remove('navMediaHidden');
+          }
+
+    }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const loadingScreen = document.querySelector('.loadingScreen');
+    loadingScreen.classList.add('loadingScreenHidden');
+    document.body.classList.remove('loading');
+
+  });
