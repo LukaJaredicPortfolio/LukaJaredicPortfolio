2c591d1ea63b74ed0cf5992552b5a7144c5ccd2d
LukaJaredicPortfolio <155008848+LukaJaredicPortfolio@users.noreply.github.com>
2024-01-16 01:12:50 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -25,27 +25,5 @@ window.addEventListener('scroll', function() {
     }
 });
 
-function pauseVideo(video) {
-    video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-  }
+
   
-  // Create an intersection observer
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      // If the video is not intersecting with the viewport, pause it
-      if (entry.isIntersecting) {
-        // Video is in the viewport, do nothing
-      } else {
-        // Video is not in the viewport, pause it
-        pauseVideo(entry.target.querySelector('iframe'));
-      }
-    });
-  }, { threshold: 0.5 }); // Adjust the threshold as needed
-  
-  // Get the video wrapper element
-  const videoWrapper = document.querySelector('.youtubeVideoWrapper');
-  
-  // Observe the video wrapper
-  if (videoWrapper) {
-    observer.observe(videoWrapper);
-  }
